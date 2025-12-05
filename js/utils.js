9c4d6ee3a144aaa2570fa86e8c7cde56b338039d
lambchop38 <arthurlam008@gmail.com>
2025-12-05 12:41:17 -0800

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -19,7 +19,7 @@ function determineWinner({player, enemy, timerId}) {
         }
 }
 
-let timer = 10
+let timer = 60
 let timerId
 function decreaseTimer() {
     timerId = setTimeout(decreaseTimer, 1000)
